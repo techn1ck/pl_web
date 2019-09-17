01f49b04dc960af43e3fc9dd291e1dc07da45ec2
Nikita Yakovlev <nikita.uventa@gmail.com>
2019-09-17 22:29:12 +0300

    Пример 1. Запрос погоды

---
+++ weather.py
@@ -0,0 +1,26 @@
+import requests
+import settings
+
+def get_weather_by_city(city_name):
+    weather_url = 'http://api.worldweatheronline.com/premium/v1/weather.ashx'
+    params = {
+        'key' : settings.API_KEY,
+        'q' : city_name,
+        'format' : 'json',
+        'num_of_days' : 1,
+        'lang' : 'ru',
+    }
+    result = requests.get(weather_url,  params=params)
+    weather = result.json() 
+    if 'data' in weather:
+        if 'current_condition' in weather['data']:
+            try:
+                return weather['data']['current_condition'][0]
+            except (IndexError, TypeError):
+                return False
+    return False
+
+
+if __name__ == '__main__':
+    w = get_weather_by_city('Sankt-Peterburg, Russia')
+    print(w)
